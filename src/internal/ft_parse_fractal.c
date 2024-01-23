9e630ce5bb495438728eec72dbf3072de9793341
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2024-01-23 02:54:38 -0300

    Fix: move the code of the main to the ft_parse_fractal.  Changes to be committed: 	modified:   bonus/internal/ft_parse_fractal_bonus.c 	modified:   bonus/main.c 	modified:   src/internal/ft_parse_fractal.c 	modified:   src/main.c

--- src/internal/ft_parse_fractal.c
+++ src/internal/ft_parse_fractal.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:54:16 by dande-je          #+#    #+#             */
-/*   Updated: 2024/01/22 08:48:50 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/01/23 02:51:11 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ static void	ft_parse_julia(char **map);
 
 void	ft_parse_fractal(int32_t argc, char **map)
 {
+	if (argc < 2 || argc > 4)
+		ft_output_error("Invalid Fractal", \
+			"./fractol [fractal type] [fractal constant]");
 	if (!ft_strncmp(map[1], "julia", ft_str_len("julia")))
 		ft_parse_julia(map);
 	else if (!ft_strncmp(map[1], "mandelbrot", ft_str_len("mandelbrot")))
